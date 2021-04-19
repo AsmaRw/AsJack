f97275c32ca1d7d6da6d4d1ba9c499c10236e3ca
nyou <asma.aourachi@gmail.com>
2021-04-19 17:31:12 +0200

    component

---
+++ blackjack/src/Table.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Dealer from './componant/Play/Dealer.jsx'
+//import Player from './componant/Play/Player.jsx'
+
+class Table extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>test</h1>
+
+      </div>
+    );
+  }
+}
+
+export default Table;
+
---
+++ blackjack/src/componant/Play/Dealer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+//import Cartes from '../carte/Cartes.jsx'
+
+class Dealer extends React.Component {
+  render() {
+    return (
+      <div>
+     
+      </div>
+    );
+  }
+}
+
+export default Dealer;
