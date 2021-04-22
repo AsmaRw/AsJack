01419afb92d535a09a041f6b5f173cce698548c2
nyou <asma.aourachi@gmail.com>
2021-04-22 17:32:47 +0200

    fini

--- blackjack/src/Table.js
+++ blackjack/src/Table.js
@@ -38,7 +38,6 @@ class Table extends React.Component {
   }
 
   rndCarte() {
-
     arrayLength = + this.state.playerCardList.length;
 
     rndNumTemp = Math.floor(Math.random() * 53);
@@ -46,55 +45,85 @@ class Table extends React.Component {
     if (rndNumTemp > 52) { rndNumTemp = rndNumTemp - 10 } else if (rndNumTemp < 1) { rndNumTemp = rndNumTemp + 10 }
 
     rndCarteTemp = cardArray[rndNumTemp - 1];
-    console.log("rndCarteTemp", rndCarteTemp)
 
     return rndCarteTemp
   }
+
   onClickStop = () => {
-    this.setState({ starDealer: true })
     const cardSelectedDealer = this.rndCarte()
+    const cardSelectedDealer2 = this.rndCarte()
 
     const valueCarteDealer = this.transformCardIntoInt(cardSelectedDealer.split("")[0])
-    const totalValueDealer = this.state.counterDealer + valueCarteDealer
+    const valueCarteDealer2 = this.transformCardIntoInt(cardSelectedDealer2.split("")[0])
+
+    const cardsDealer = [cardSelectedDealer, cardSelectedDealer2]
+
+    let dealerValue = valueCarteDealer + valueCarteDealer2
+
+    let endGameAndWinner = {
+      endGame: false,
+      nameOfWinner: ""
+    }
+
+    while (dealerValue < 17) {
+      const cardSelectedDealer = this.rndCarte()
+      const valueCarteDealer = this.transformCardIntoInt(cardSelectedDealer.split("")[0])
+
+      cardsDealer.push(cardSelectedDealer)
+
+      dealerValue += valueCarteDealer
+
+      if (dealerValue > 21) {
+
+        // endGameAndWinner = this.calculateCard(dealerValue, "Dealer")
+        endGameAndWinner = {
+          endGame: true,
+          nameOfWinner: "Player"
+        }
+
+        break;
+      }
+    }
+    if (dealerValue <= 21) {
+      if (this.state.counterPlayer > 21) {
+        endGameAndWinner = {
+          endGame: true,
+          nameOfWinner: "Dealer"
+        }
+      } else if (this.state.counterPlayer < dealerValue) {
+        endGameAndWinner = {
+          endGame: true,
+          nameOfWinner: "Dealer"
+        }
+      } else {
+        endGameAndWinner = {
+          endGame: true,
+          nameOfWinner: "Player"
+        }
+      }
+    }
+
+    // une condition pour verifier le plus grand 
+
+    console.log("update state on stop");
 
     this.setState({
-      counterDealer: totalValueDealer,
-      dealerCardList: [...this.state.dealerCardList, cardSelectedDealer]
+      counterDealer: dealerValue,
+      dealerCardList: cardsDealer,
+      nameOfWinner: endGameAndWinner.nameOfWinner,
+      endGame: endGameAndWinner.endGame
     })
-
-    this.calculateCard(totalValueDealer)
-    console.log("this.state.counterDealer", this.state.counterDealer);
-    console.log("totalValueDealer", totalValueDealer);
   }
 
   onClickGive = () => {
-
     const cardSelected = this.rndCarte()
-    console.log('player card :', cardSelected)
-    const cardSelectedDealer = this.rndCarte()
-    console.log('dealer card :', cardSelectedDealer)
-
-    //console.log(this.rndCarte())
-
     const valueCarte = this.transformCardIntoInt(cardSelected.split("")[0])
     const totalPlayerValue = this.state.counterPlayer + valueCarte
-
 
     this.setState({
       counterPlayer: totalPlayerValue,
       playerCardList: [...this.state.playerCardList, cardSelected]
     })
-
-    console.log('DealerCardList :', this.state.dealerCardList)
-
-    console.log("totalplayervalue", totalPlayerValue); console.log("totalValueDealer", totalValueDealer)
-    console.log("totalplayervalue", this.state.counterPlayer); console.log("totalValueDealer", this.state.counterDealer)
-
-
-    // this.calculateCard(totalPlayerValue)
-    // this.calculateCard(totalValueDealer)
-    this.calculateCard(totalPlayerValue, "Player")
-    this.calculateCard(totalValueDealer, "Dealer")
   }
 
   transformCardIntoInt(cardValue) {
@@ -106,44 +135,82 @@ class Table extends React.Component {
   }
 
   startGame = () => {
+    const cardSelected = this.rndCarte()
+    const cardSelected2 = this.rndCarte()
+
+    const valueCarte = this.transformCardIntoInt(cardSelected.split("")[0])
+    const valueCarte2 = this.transformCardIntoInt(cardSelected2.split("")[0])
+
+    const firstPlayerValue = valueCarte + valueCarte2
+
+    const firstTwoCardsPlayer = [cardSelected, cardSelected2]
+
+    //Bonus vérifier si il a Blackjack et donc il a gagné d'un coup
+
     this.setState({
+      counterPlayer: firstPlayerValue,
+      playerCardList: firstTwoCardsPlayer,
       startGame: true
     })
   }
 
+  // calculateCard(value, name) {
+  //   console.log("value in calculatecard", value)
+  //   console.log('calculateCard name ', name)
+  //   console.log('calculateCard value ', value)
 
-  calculateCard(value, name) {
-    console.log("value in calculatecard", value)
-    if (value > 21) {
-      // console.log("Busted")
-      this.setState({
-        endGame: true,
-        nameOfWinner: name == "Dealer" ? "Player" : "Dealer"
-      })
-      return
-    } else if(value === 21) {
-      // console.log("Busted")
-      this.setState({
-        endGame: true,
-        nameOfWinner: name
-      })
-      return
-    } 
-    // } else if (value <= 16) {
-    //   console.log(name, "Take card and continue the game...")
-    // } else if (value > 16 && value < 21) {
-    //   console.log(name, "Do you want to Stand or Hit and continue the game?")
-    // }
-  }
-  rendertable() {
-    if (this.state.counterPlayer > 21 || this.state.starDealer == true) {
+  //   if (value > 21) {
+  //     console.log("1 er condition")
+
+  //     // this.setState({
+  //     //   endGame: true,
+  //     //   nameOfWinner: name == "Dealer" ? "Player" : "Dealer"
+  //     // })
+
+  //     return {
+  //       endGame: true,
+  //       nameOfWinner: name == "Dealer" ? "Player" : "Dealer"
+  //     }
+
+  //   } else if (value == 21) {
+  //     console.log("2e condition")
+
+  //     // this.setState({
+  //     //   endGame: true,
+  //     //   nameOfWinner: name
+  //     // })
+
+  //     return {
+  //       endGame: true,
+  //       nameOfWinner: name
+  //     }
+  //   }
+  //   // else {
+  //   //   return {
+  //   //     endGame: false,
+  //   //     nameOfWinner: ""
+  //   //   }
+  //   // }
+  // }
+
+  render() {
+    if (this.state.startGame == false) {
       return (
-        <div>
+        <Game startGame={this.startGame} />
+      )
+    } else {
+      return (<div>
+
+        <div className="playGame">
           <div style={{ height: '100vh', position: 'relative' }}>
             <h1 style={{ color: '#feb236', textAlign: 'center' }}>Black Jack</h1>
-            {/* <img src='https://m.media-amazon.com/images/I/71g6q+jPYAL._AC_UL320_.jpg' alt="W3Schools" width="104" height="142" /> */}
-            <DealerCard cardSelectedDealer={this.state.dealerCardList} />
-            <Cartes cardList={this.state.playerCardList} />
+            {/* <DealerCard cardSelectedDealer={this.state.dealerCardList} /> */}
+            <Cartes key={"dealer"} cardList={this.state.dealerCardList} />
+            {this.state.endGame && (<div className='winlost'>
+              <h1>Winner is {this.state.nameOfWinner}</h1>
+            </div>)}
+            <Cartes key={"player"} cardList={this.state.playerCardList} />
+
             <div style={{ bottom: '20px', position: 'absolute' }} className="row col-6 offset-3 flex d-flex justify-content-between">
               <div className="d-grid gap-2">
                 <Button
@@ -155,7 +222,6 @@ class Table extends React.Component {
                 />
               </div>
               <div>
-                {/* <Cartes cardList={this.state.playerCardList} /> */}
               </div>
               <div className="d-grid gap-2">
                 <Button
@@ -170,68 +236,8 @@ class Table extends React.Component {
             </div>
           </div>
         </div>
+      </div>
       )
-    } else {
-      return ( <div>
-        {this.state.endGame ? 
-          (<div>
-            <h1>Winner is {this.state.nameOfWinner}</h1>
-          </div>) 
-          : (
-        <div className="playGame">
-              <div style={{ height: '100vh', position: 'relative' }}>
-                <h1 style={{ color: '#feb236', textAlign: 'center' }}>Black Jack</h1>
-                {/* <img src='https://m.media-amazon.com/images/I/71g6q+jPYAL._AC_UL320_.jpg' alt="W3Schools" width="104" height="142" /> */}
-
-                <DealerCard cardSelectedDealer={this.state.dealerCardList} />
-                <Cartes cardList={this.state.playerCardList} />
-
-                <div style={{ bottom: '20px', position: 'absolute' }} className="row col-6 offset-3 flex d-flex justify-content-between">
-                  <div className="d-grid gap-2">
-                    <Button
-                      onClick={this.onClickGive}
-                      classe="btn btn-outline-warning btn-lg"
-                      color="white"
-                      bcolor="rgba(18, 102, 241, 0.7)"
-                      name="give"
-                    />
-                  </div>
-                  <div>
-                    {/* <Cartes cardList={this.state.playerCardList} /> */}
-                  </div>
-                  <div className="d-grid gap-2">
-                    <Button
-                      onClick={this.onClickStop}
-                      classe="btn btn-outline-danger btn-lg"
-                      color="white"
-                      bcolor="rgba(178, 60, 253, 0.5)"
-                      name="stop"
-                    />
-                  </div>
-
-                </div>
-              </div>
-            </div>
-          )
-        }
-        </div>
-      )
-    }
-  }
-
-  render() {
-    console.log('counterPlayer', this.state.counterPlayer);
-    console.log('counterDealer', this.state.counterDealer);
-    if (this.state.startGame == false) {
-      return (
-        <Game startGame={this.startGame} />
-      )
-    } else {
-      return (
-        <div>
-          {this.rendertable()}
-        </div>
-      );
     }
   }
 }
